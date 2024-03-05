053ac4683e849a7e2207216ce8655622485f420f
Eda Derya Toper <edaderyatoper@gmail.com>
2024-03-06 01:02:53 +0300

    End of section 10

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -2,23 +2,31 @@
 
 import { Auction, PagedResult } from '@/types';
 import { getTokenWorkaround } from './authActions';
+import { fetchWrapper } from '@/lib/fetchWrapper';
+import { FieldValues } from 'react-hook-form';
+import { revalidatePath } from 'next/cache';
 
 //API'dan veriyi çekme json formatında return etme
 
 export async function getData(query: string):Promise<PagedResult<Auction>>{
     //getData(pageNumber: number, pageSize:number)
     //`http://localhost:6001/searches/SearchWithHelper?PageSize=${pageSize}&pageNumber=${pageNumber}`
-    const res = await fetch(`http://localhost:6001/searches/SearchWithHelper${query}`);
+    
+    /*const res = await fetch(`http://localhost:6001/searches/SearchWithHelper${query}`);
 
     if(!res.ok) throw new Error('Failed to fetch data');
-    return res.json();
+    return res.json();*/
+    return await fetchWrapper.get(`searches/SearchWithHelper${query}`)
 }
 
 export async function updateAuctionTest() {
     const data = {
         mileage: Math.floor(Math.random() * 100000) + 1
     }
-    const token = await getTokenWorkaround();
+    return await fetchWrapper.put('auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c',data)
+
+    //!Aşağıdaki kısım fetchWrapper içerisinde yer alıyor zaten
+    /*const token = await getTokenWorkaround();
 
     const res = await fetch('http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c',{
         method: 'PUT',
@@ -30,5 +38,19 @@ export async function updateAuctionTest() {
     })
 
     if (!res.ok) return{status:res.status, message:res.statusText}
-    return res.statusText;
+    return res.statusText;*/
+}
+export async function createAuction(data: FieldValues) {
+    return await fetchWrapper.post('auctions', data);
+}
+export async function getDetailedViewData(id: string): Promise<Auction> {
+    return await fetchWrapper.get(`auctions/${id}`);
+}
+export async function updateAuction(data: FieldValues, id: string) {
+    const res = await fetchWrapper.put(`auctions/${id}`, data);
+    revalidatePath(`/auctions/${id}`);
+    return res;
+}
+export async function deleteAuction(id: string) {
+    return await fetchWrapper.del(`auctions/${id}`);
 }
--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import CountdownTimer from './CountdownTimer'
 import CarImage from './CarImage'
 import { Auction } from '@/types'
+import Link from 'next/link'
 
 type Props={
     auction:Auction
@@ -10,7 +11,7 @@ type Props={
 // AuctionCard bileşeni, aldığı Listingste aldığı auction nesnesinin "make" özelliğini ekrana basar.
 export default function AuctionCard({auction}:Props) {
   return (
-    <a href='#' className='group'>
+    <Link href={`/auctions/details/${auction.id}`} className='group'>
         <div className='w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden'>
             <div>
            <CarImage imageUrl={auction.imageUrl}/>
@@ -27,7 +28,7 @@ export default function AuctionCard({auction}:Props) {
         </div>
         
 
-    </a>
+    </Link>
    //<div>{auction.make}</div>
   )
 }
